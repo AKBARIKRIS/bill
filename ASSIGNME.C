36137393bfbe1e84a7e10a95e8d60e7a759676b8
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-06-29 05:20:04 -0700

    Add files via upload

--- ASSIGNME.C
+++ ASSIGNME.C
@@ -2,31 +2,46 @@
 #include<conio.h>
 
 void main()
-{ int bill,total_bill,sc,unit;
+{
+  char M,T,W,t,F,S,s;
   clrscr();
-  printf("enter use unit =");
-  scanf("%d",&unit);
-  if (unit<=50)
+  char c;
+  printf("press key =M \n");
+  printf("press key =T \n");
+  printf("press key =W \n");
+  printf("press key =t \n");
+  printf("press key =F \n");
+  printf("press key =S \n");
+  printf("press key =s \n");
+  printf("enter first letar of =");
 
-    {
-	bill=unit*0.50;
-    }
-  else if (unit<=150)
-    {
-	bill=(unit-50)*0.75+25;
-    }
-  else if (unit<=250)
-    {
-	bill=(unit-150)*1.20+100;
-    }
-  else
-    {
-	bill=(unit-250)*1.50+220;
-    }
-
-    sc=bill*20/100;
-    printf("your surcharge is=%d\n",sc);
-    total_bill=sc+bill;
-    printf("you pay bill is =%d",total_bill);
-    getch();
-    }
+  scanf("%c",&c);
+  switch(c)
+  {
+	case  'M'  :
+	       printf("day 1 MONDAY ",M);
+	       break;
+	case 'T' :
+	       printf("day 2 TUSEDAY ",T);
+	       break;
+	case 'W' :
+	       printf("day 3 WEDNESDAY ",W);
+	       break;
+	case 't' :
+	       printf("day 4 THURSDAY",t);
+	       break;
+	case 'F' :
+	       printf("day 5 FRIDAY",F);
+	       break;
+	case 'S' :
+	       printf("day 6 SATURDAY ",S);
+	       break;
+	case 's' :
+	       printf("day 7 SUNDAY",s);
+	       break;
+	default:
+		printf("plz enter proparly ");
+		break;
+  }
+  getch();
+}
